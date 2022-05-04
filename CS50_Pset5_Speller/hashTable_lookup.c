da7720705a88f87ae74b708096e058e30af769d5
Eric Cheng <jhchengeric@gmail.com>
2022-05-04 09:11:15 +0800

    add notes

--- CS50_Pset5_Speller/hashTable_lookup.c
+++ CS50_Pset5_Speller/hashTable_lookup.c
@@ -5,7 +5,6 @@
 #include <stdbool.h>
 
 #define MAX_NAME 256
-//size of hash_table
 #define TABLE_SIZE 10
 
 typedef struct{
@@ -51,17 +50,13 @@ void init_hash_table()
 //STEP4_Print out hash table
 void print_table()
 {
-  //delcare beginning of the hash table
   printf("Start\n");
-  //loop through hash table
   for (int i = 0; i < TABLE_SIZE; i++)
   {
-    //nothing in hash table
     if(hash_table == NULL)
     {
       printf("%i ---\n",i);
     }
-    //elements in hash table
     else
     {
       printf(" %i %s\n", i, hash_table[i]->name);
@@ -71,27 +66,37 @@ void print_table()
 }
 
 //STEP5_Insertion to hash table
-/*
-this hash table insert address of person's name using bool
-*/
 bool hash_table_insertion(person *p)
 {
-  //no person
   if (p == NULL)
   {
     return false;
   }
-  //is person
   else{
     int index = hash(p->name);
-    //hash_table[index] filled: collision
+    //check availability of the hash_table[index]
     if(hash_table[index] != NULL)
     {
       return false;
     }
-    //hash_table[index] avaiable
+    //insert p into hash_table[index]
     hash_table[index] = p;
     return true;
+  }
+}
+
+//STEP6_Look up_Find a person by name
+person *hash_table_lookup(char *name)
+{
+  int index = hash(name);
+  if (hash_table[index] != NULL && 
+  strcmp(hash_table[index]->name, name) == 0)
+  {
+    return hash_table[index];
+  }
+  else
+  {
+    return NULL;
   }
 }
 
@@ -100,27 +105,24 @@ int main()
   init_hash_table();
   print_table();
 
-  person jacob = {.name = "Jacob", .age = 256};
+  person jacob = {.name = "jacob", .age = 256};
   person kate = {.name = "kate", .age = 27};
   person mpho = {.name = "mpho", .age = 14};
 
   hash_table_insertion(&jacob);
   hash_table_insertion(&kate);
   hash_table_insertion(&mpho);
-
   print_table();
 
-  /*
-  printf("Jacob => %u\n", hash("Jacob"));
-  printf("Natalie => %u\n", hash("Natalie"));
-  printf("Sara => %u\n", hash("Sara"));
-  printf("Mpho => %u\n", hash("Mpho"));
-  printf("Tebogo => %u\n", hash("Tebogo"));
-  printf("Ron => %u\n", hash("Ron"));
-  printf("Jane => %u\n", hash("Jane"));
-  printf("Maren => %u\n", hash("Maren"));
-  printf("Bill => %u\n", hash("Bill"));
-  */
+  person *tmp = hash_table_lookup("jacob");
+  if(tmp == NULL)
+  {
+    printf("Not found!");
+  }
+  else
+  {
+    printf("Found!");
+  }
 
   return 0;
 }
