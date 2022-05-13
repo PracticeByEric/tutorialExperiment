0fcf8e656c81dda5995b8ea5ad1f72044719a5da
Eric Cheng <jhchengeric@gmail.com>
2022-05-13 16:05:18 +0800

    division and float number

---
+++ Python/division.py
@@ -0,0 +1,14 @@
+try:
+  x = int(input("x: "))
+except ValueError:
+  print("Not an int!")
+  exit()
+
+try:
+  y = int(input("y: "))
+except ValueError:
+  print("Not an int!")
+  exit()
+
+z = x / y
+print(f"{z:.50f}")
