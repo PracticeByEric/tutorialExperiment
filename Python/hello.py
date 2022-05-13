abf46b44d759748097bfde127a24aecf8500f172
Eric Cheng <jhchengeric@gmail.com>
2022-05-13 15:03:58 +0800

    add input in hello

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,7 @@
-print("Hello, world")
+#A program to say 'hello world' in python
+
+# print("Hello, world")
+
+#A program to ask for name and say hello to the person
+#answer = input("What's your name? ");
+#print(f"hello, {answer}");
