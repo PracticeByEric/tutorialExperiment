73d7c49657953bf11a4fa422ebbe7262b8e1d278
Eric Cheng <jhchengeric@gmail.com>
2022-05-14 09:02:49 +0800

    create a function in python

---
+++ Python/create_function.py
@@ -0,0 +1,20 @@
+#To create a function in Python and implement
+#define a main
+# def main():
+#   for i in range(3):
+#     meow()
+
+# def meow():
+#   print("meow")
+
+# main()
+
+#------>> customize times meowed <<--------
+def main():
+    meow(3)
+
+def meow(n):
+    for i in range(n):
+        print("meow")
+
+main()
