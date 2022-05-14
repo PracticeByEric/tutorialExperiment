0c6fa7496c1d991f495c2237409ce096d3a9dba8
Eric Cheng <jhchengeric@gmail.com>
2022-05-14 16:10:31 +0800

    search in Python

---
+++ Python/names.py
@@ -0,0 +1,13 @@
+#linear search for strings
+#relacing loops in C
+
+import sys
+
+names = ["Bill", "Charlie", "Fred", "George"]
+
+if "George" in names:
+  print("Found")
+  sys.exit(0)
+
+print("Not found")
+sys.exit(1)
