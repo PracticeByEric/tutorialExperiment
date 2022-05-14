af1bc7676b2bd1a2e8d6233dee98a283082c1290
Eric Cheng <jhchengeric@gmail.com>
2022-05-14 16:10:56 +0800

    search for numbers

---
+++ Python/numbers.py
@@ -0,0 +1,12 @@
+# linear search of numbers
+import sys
+
+numbers = [4, 6, 8, 2, 7, 5, 0]
+
+if 0 in numbers: #linear search
+  print("Found")
+  sys.exit(0)
+
+else:
+  print("Not found")
+  sys.exit(1)
